18bb31d80cc97f3744f6f6d20037eaf5d0a460f6
meshkatKanok <meshkatkanok12@gmail.com>
2023-02-06 13:47:27 +0600

    nextjs

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,23 @@
+import Banner from './Components/Banner/Banner'
+import Bestfull from './Components/Bestfull/Bestfull'
+import RespponsiveBestfull from './Components/Bestfull/RespponsiveBestfull'
+import Header from './Components/Header/Header'
+import HealthySide from './Components/Midpart/HealthySide'
+import SectionThree from './Components/SectionThree/SectionThree'
 import './globals.css'
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
-    </html>
+     <div>
+      <div className='lg:w-[1440px] lg:h-[4919px] mx-auto w-[100%] h-[6623px]'>
+      <Header/>
+       <Banner/>
+       <HealthySide/>
+       <SectionThree/>
+       <Bestfull/>
+       <RespponsiveBestfull/>
+
+      </div>
+     </div>
   )
 }
